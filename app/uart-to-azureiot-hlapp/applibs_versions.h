69c9dfc0a1bf3c2888447ae4ae23d7a0130f768f
Takashi Matsuoka <matsujirushi@live.jp>
2020-07-25 22:03:20 +0900

    1st

---
+++ app/uart-to-azureiot-hlapp/applibs_versions.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+/// <summary>
+/// この識別子は、ネットワーク関連のヘッダー ファイルを含める前に定義する必要があります。
+/// これは、アプリケーションが使用する Wi-Fi データ構造のバージョンを示します。
+/// </summary>
+#define NETWORKING_STRUCTS_VERSION 1
+
+/// <summary>
+/// この識別子は、Wi-Fi 関連のヘッダー ファイルを含める前に定義する必要があります。
+/// これは、アプリケーションが使用する Wi-Fi データ構造のバージョンを示します。
+/// </summary>
+#define WIFICONFIG_STRUCTS_VERSION 1
+
+/// <summary>
+/// この識別子は、UART 関連のヘッダー ファイルを含める前に定義する必要があります。
+/// これは、アプリケーションが使用する UART データ構造のバージョンを示します。
+/// </summary>
+#define UART_STRUCTS_VERSION 1
+
+/// <summary>
+/// この識別子は、SPI 関連のヘッダー ファイルを含める前に定義する必要があります。
+/// これは、アプリケーションが使用する SPI データ構造のバージョンを示します。
+/// </summary>
+#define SPI_STRUCTS_VERSION 1
