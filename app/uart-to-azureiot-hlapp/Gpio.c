d818f3100fd30c54437e3ae20ea1828332dcfab2
Takashi Matsuoka <matsujirushi@live.jp>
2020-08-12 22:41:07 +0900

    Add Message Parser

--- app/uart-to-azureiot-hlapp/Gpio.c
+++ app/uart-to-azureiot-hlapp/Gpio.c
@@ -7,7 +7,7 @@
 bool GpioRead(int gpioFd)
 {
 	GPIO_Value_Type value;
-	int result = GPIO_GetValue(gpioFd, &value);
+	const int result = GPIO_GetValue(gpioFd, &value);
 	if (result != 0) return false;
 
 	return value == GPIO_Value_High;
@@ -16,7 +16,7 @@ bool GpioRead(int gpioFd)
 bool GpioReadInv(int gpioFd)
 {
 	GPIO_Value_Type value;
-	int result = GPIO_GetValue(gpioFd, &value);
+	const int result = GPIO_GetValue(gpioFd, &value);
 	if (result != 0) return true;
 
 	return value == GPIO_Value_Low;
--- app/uart-to-azureiot-hlapp/main.c
+++ app/uart-to-azureiot-hlapp/main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <assert.h>
 #include "Exit.h"
 #include "Termination.h"
 #include "Gpio.h"
@@ -51,7 +52,10 @@ static EventLoop* EvLoop = NULL;
 static EventRegistration* EvRegUart = NULL;
 static EventLoopTimer* EvTimerButton = NULL;
 
-static void SendUartMessage(int uartFd, const char* dataToSend)
+////////////////////////////////////////////////////////////////////////////////
+// Uart Send
+
+static void UartSend(int uartFd, const char* dataToSend)
 {
     size_t totalBytesSent = 0;
     const size_t totalBytesToSend = strlen(dataToSend);
@@ -61,7 +65,7 @@ static void SendUartMessage(int uartFd, const char* dataToSend)
 
         size_t bytesLeftToSend = totalBytesToSend - totalBytesSent;
         const char* remainingMessageToSend = dataToSend + totalBytesSent;
-        ssize_t bytesSent = write(uartFd, remainingMessageToSend, bytesLeftToSend);
+        const ssize_t bytesSent = write(uartFd, remainingMessageToSend, bytesLeftToSend);
         if (bytesSent == -1) {
             Exit_DoExitWithLog(ExitCode_SendMessage_Write, "ERROR: write() - %s (%d)\n", strerror(errno), errno);
             return;
@@ -70,7 +74,7 @@ static void SendUartMessage(int uartFd, const char* dataToSend)
         totalBytesSent += (size_t)bytesSent;
     }
 
-    Log_Debug("Sent %zu bytes over UART in %d calls.\n", totalBytesSent, sendIterations);
+    Log_Debug("Sent %zu bytes in %d calls.\n", totalBytesSent, sendIterations);
 }
 
 static void ButtonTimerEventHandler(EventLoopTimer* timer)
@@ -81,26 +85,77 @@ static void ButtonTimerEventHandler(EventLoopTimer* timer)
     }
 
     const bool newButtonValue = GpioReadInv(ButtonFd);
-    if (!ButtonValue && newButtonValue) SendUartMessage(UartFd, "Hello world!\n");
+    if (!ButtonValue && newButtonValue) UartSend(UartFd, "label1:11,label2:22\n");
     ButtonValue = newButtonValue;
 }
 
-static void UartEventHandler(EventLoop* el, int fd, EventLoop_IoEvents events, void* context)
-{
-    uint8_t receiveBuffer[UART_RECEIVE_BUFFER_SIZE + 1];
-    ssize_t bytesRead;
+////////////////////////////////////////////////////////////////////////////////
+// Message Parser
 
-    bytesRead = read(UartFd, receiveBuffer, UART_RECEIVE_BUFFER_SIZE);
-    if (bytesRead == -1) {
+static uint8_t MessageParserInputBuffer[256];
+static size_t MessageParserInputBufferVaildSize = 0;
+
+static uint8_t* MessageParserGetInputBufferBegin(void)
+{
+    return MessageParserInputBuffer + MessageParserInputBufferVaildSize;
+}
+
+static uint8_t* MessageParserGetInputBufferEnd(void)
+{
+    return MessageParserInputBuffer + sizeof(MessageParserInputBuffer) / sizeof(MessageParserInputBuffer[0]);
+}
+
+static void MessageParserAddValidInputSize(size_t size)
+{
+    MessageParserInputBufferVaildSize += size;
+    assert(MessageParserInputBufferVaildSize <= sizeof(MessageParserInputBuffer) / sizeof(MessageParserInputBuffer[0]));
+}
+
+static void MessageParserDoWork(void)
+{
+    uint8_t* begin;
+    uint8_t* end;
+    do {
+        begin = MessageParserInputBuffer;
+        end = MessageParserInputBuffer + MessageParserInputBufferVaildSize;
+
+        for (; begin != end; ++begin) {
+            if (*begin == 0x0a) {
+                Log_Debug("[");
+                for (uint8_t* p = MessageParserInputBuffer; p != begin; ++p) Log_Debug("%c", *p);
+                Log_Debug("]\n");
+
+                MessageParserInputBufferVaildSize = (size_t)(end - (begin + 1));
+                memmove(MessageParserInputBuffer, begin + 1, MessageParserInputBufferVaildSize);
+                break;
+            }
+        }
+    } while (begin != end);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Uart Receive
+
+static void UartReceiveHandler(EventLoop* el, int fd, EventLoop_IoEvents events, void* context)
+{
+    uint8_t* begin = MessageParserGetInputBufferBegin();
+    uint8_t* end = MessageParserGetInputBufferEnd();
+    assert(begin != end);
+    const ssize_t readSize = read(UartFd, begin, (size_t)(end - begin));
+    if (readSize == -1) {
         Exit_DoExitWithLog(ExitCode_UartEvent_Read, "ERROR: read() - %s (%d)\n", strerror(errno), errno);
         return;
     }
 
-    if (bytesRead > 0) {
-        receiveBuffer[bytesRead] = 0;
-        Log_Debug("UART received %d bytes: '%s'.\n", bytesRead, (char*)receiveBuffer);
+    if (readSize > 0) {
+        Log_Debug("readSize = %d\n", readSize);
+        MessageParserAddValidInputSize((size_t)readSize);
+        MessageParserDoWork();
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Init & Close
 
 static void InitPeripheralsAndHandlers(void)
 {
@@ -132,7 +187,7 @@ static void InitPeripheralsAndHandlers(void)
         Exit_DoExitWithLog(ExitCode_Init_ButtonPollTimer, "ERROR: CreateEventLoopPeriodicTimer() - %s (%d)\n", strerror(errno), errno);
         return;
     }
-    EvRegUart = EventLoop_RegisterIo(EvLoop, UartFd, EventLoop_Input, UartEventHandler, NULL);
+    EvRegUart = EventLoop_RegisterIo(EvLoop, UartFd, EventLoop_Input, UartReceiveHandler, NULL);
     if (EvRegUart == NULL) {
         Exit_DoExitWithLog(ExitCode_Init_RegisterIo, "ERROR: EventLoop_RegisterIo() - %s (%d)\n", strerror(errno), errno);
         return;
@@ -148,6 +203,9 @@ static void ClosePeripheralsAndHandlers(void)
     if (UartFd >= 0) close(UartFd);
     if (ButtonFd >= 0) close(UartFd);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// main
 
 int main(int argc, char* argv[])
 {
@@ -167,3 +225,5 @@ int main(int argc, char* argv[])
 
     return exitCode;
 }
+
+////////////////////////////////////////////////////////////////////////////////
