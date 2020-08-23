4c1edf460f4da0f7595ee43ef935b65307d07689
Takashi Matsuoka <matsujirushi@live.jp>
2020-08-23 15:09:16 +0900

    Add sample code for arduino

---
+++ examples/arduino/telemetry/telemetry.ino
@@ -0,0 +1,38 @@
+// Copyright (c) 2020 matsujirushi
+// Released under the MIT license
+// https://github.com/matsujirushi/uart-to-azureiot/blob/master/LICENSE.txt
+
+static constexpr int TEMPERATURE_MIN = 10;
+static constexpr int TEMPERATURE_MAX = 40;
+static constexpr int HUMIDITY_MIN = 0;
+static constexpr int HUMIDITY_MAX = 100;
+
+static constexpr char PART_SEPARATOR = '\t';
+
+static constexpr int INTERVAL = 5000; // [msec.]
+
+void setup()
+{
+  Serial.begin(115200, SERIAL_8N1);
+}
+
+void loop()
+{
+  float temperature;
+  float humidity;
+  GetTemperatureAndHumidityRandom(&temperature, &humidity);
+  
+  String message;
+  message += "temperature:" + String(temperature, 1);
+  message += PART_SEPARATOR;
+  message += "humidity:" + String(humidity, 0);
+  Serial.println(message);
+  
+  delay(INTERVAL);
+}
+
+void GetTemperatureAndHumidityRandom(float* temperature, float* humidity)
+{
+  *temperature = (float)random(TEMPERATURE_MIN * 10, TEMPERATURE_MAX * 10 + 1) / 10.0f;
+  *humidity = (float)random(HUMIDITY_MIN, HUMIDITY_MAX + 1);
+}
